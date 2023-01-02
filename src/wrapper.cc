08aa4225155edb067754adc6e1641f3e7ebb0299
Jarl <jarl_sondre@hotmail.com>
2023-01-02 13:53:17 +0100

    move binding logic to different file

---
+++ src/wrapper.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <typeinfo> 
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "pawn.h"
+#include "piece.h"
+#include "board.h"
+
+using namespace std; 
+
+// Code used to bind the C++ code to Python
+
+#include "pybind11/pybind11.h"
+namespace py = pybind11; 
+PYBIND11_MODULE(chess_module, handle) {
+    handle.doc() = "This is the module docs. Teehee"; 
+
+    py::class_<Pawn>(handle, "PyPawn")
+        .def(py::init<char>())
+        .def("getNumMoves", &Pawn::getNumMoves)
+        .def("getColor", &Piece::getColor); 
+
+    py::class_<Piece>(handle, "PyPiece")
+        .def("getColor", &Piece::getColor) 
+        .def("getNumMoves", &Piece::getNumMoves); 
+
+    py::class_<Board>(handle, "PyBoard")
+        .def(py::init<>())
+        .def("getPiece", &Board::getPiece); 
+
+}
